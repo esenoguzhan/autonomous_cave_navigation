47e8864b3d5c9ac945df637d270d83f17f1556c7
mert <mert@Ubuntu2004.myguest.virtualbox.org>
2026-02-06 23:06:07 +0100

    Octomap integrated

---
+++ ros2_ws/src/octomap_mapping/octomap_server/launch/perception_mapping.launch.py
@@ -0,0 +1,54 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    use_sim_time = True 
+
+    static_tf_alias = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='depth_camera_alias',
+        arguments=[
+            '--x', '0', '--y', '0', '--z', '0',
+            '--yaw', '0', '--pitch', '0', '--roll', '0',
+            '--frame-id', 'Quadrotor/DepthCamera',
+            '--child-frame-id', 'Quadrotor/Sensors/DepthCamera'
+        ],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
+
+    pointcloud_node = Node(
+        package='depth_image_proc',
+        executable='point_cloud_xyz_node',
+        name='depth_to_pointcloud',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        remappings=[
+            ('image_rect', '/realsense/depth/image'),
+            ('camera_info', '/realsense/depth/camera_info'),
+            ('points', '/realsense/depth/points'),
+        ]
+    )
+
+    octomap_node = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        name='octomap_server',
+        output='screen',
+        parameters=[
+            {'resolution': 0.2},
+            {'frame_id': 'Quadrotor/DepthCamera'},
+            {'use_sim_time': use_sim_time}
+        ],
+        remappings=[
+            ('cloud_in', '/realsense/depth/points')
+        ]
+    )
+
+    return LaunchDescription([
+        static_tf_alias,
+        pointcloud_node,
+        octomap_node
+    ])
